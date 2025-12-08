6927f89de1085c0cd1638adeb96e98f08f8c3bb2
vjoeh <joe.hughes@email.com>
2025-12-08 07:28:08 -0500

    v0.1.633 Alternate default walls update

--- js/roomcalc.js
+++ js/roomcalc.js
@@ -1,4 +1,4 @@
-const version = "v0.1.632";  /* format example "v0.1" or "v0.2.3" - ver 0.1.1 and 0.1.2 should be compatible with a Shareable Link because ver, v0.0, 0.1 and ver 0.2 are not compatible. */
+const version = "v0.1.633";  /* format example "v0.1" or "v0.2.3" - ver 0.1.1 and 0.1.2 should be compatible with a Shareable Link because ver, v0.0, 0.1 and ver 0.2 are not compatible. */
 const isCacheImages = true; /* Images for Canvas are preloaded in case of network disruption while being mobile. Turn to false to save server downloads */
 let perfectDrawEnabled = false; /* Konva setting. Turning off helps with performance but reduces image quality of canvas.  */
 let versionQueryString;
