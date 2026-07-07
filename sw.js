a045b7c91a6b786afaefbbb534687d3c8bf6ba4e
vtjoeh <joe.hughes@gmail.com>
2026-07-07 10:06:04 -0400

    Add PWA support with service worker, manifest, and local DOMPurify
    - Added theme-color meta tag and manifest.json link for PWA configuration
    - Added apple-touch-icon link for iOS home screen support
    - Replaced CDN DOMPurify with local ./js/purify.min.js for offline capability
    - Registered service worker (./sw.js) with console logging for registration status
    - Updated favicon.ico binary
    - Bumped version to v0.1.656

---
+++ sw.js
@@ -0,0 +1,128 @@
+const CACHE_VERSION = 'v1';
+const CACHE_NAME = `vrc-pwa-${CACHE_VERSION}`;
+
+const PRECACHE_ASSETS = [
+  './',
+  './RoomCalculator.html',
+  './style.css',
+  './manifest.json',
+  './js/purify.min.js',
+  './js/konva.min.js',
+  './js/constants.js',
+  './js/roomcalc.js',
+  './js/templates.js',
+  './js/idbStorage.js',
+  './js/undoApply.js',
+  './js/colorPicker.js',
+  './js/qrcode.js',
+  './js/pocLoginCloud.js',
+  './js/migrateLegacyItemsShape.js',
+  './js/dxfWriter.js',
+  './js/dxfBlockLibrary.js',
+  './js/drpDownOverride.js',
+  './js/util/units.js',
+  './js/data/workspaceKey.js',
+  './js/data/certifiedDisplays.js',
+  './js/data/menuItemsAndMessages.js',
+  './assets/momentum-icons.css',
+  './assets/Inter-VariableFont.ttf',
+  './assets/MomentumFontIcon.woff2',
+  './assets/favicon.ico',
+  './assets/icons/icon-192.png',
+  './assets/icons/icon-512.png',
+  './assets/images/Video_Camera-front.png',
+  './assets/images/Video_Camera-top.png',
+  './assets/images/boardPro55-front.png',
+  './assets/images/boardPro55-top.png',
+  './assets/images/boardPro75-front.png',
+  './assets/images/boardPro75-top.png',
+  './assets/images/box-front.png',
+  './assets/images/brdPro55G2-front.png',
+  './assets/images/brdPro55G2-top.png',
+  './assets/images/brdPro55G2FS-front.png',
+  './assets/images/brdPro55G2FS-top.png',
+  './assets/images/brdPro75G2-front.png',
+  './assets/images/brdPro75G2-top.png',
+  './assets/images/brdPro75G2FS-front.png',
+  './assets/images/brdPro75G2FS-top.png',
+  './assets/images/brdPro75G2Wheel-top.png',
+  './assets/images/cameraP60-front.png',
+  './assets/images/cameraP60-top.png',
+  './assets/images/ceilingFan-menu.png',
+  './assets/images/ceilingFan-top.png',
+  './assets/images/ceilingGrid-menu.png',
+  './assets/images/ceilingMic-front.png',
+  './assets/images/ceilingMic-top.png',
+  './assets/images/ceilingMicPro-front.png',
+  './assets/images/ceilingMicPro-top.png',
+  './assets/images/ceilingSpeaker-menu.png',
+  './assets/images/ceilingSpeaker-top.png',
+  './assets/images/chair-front.png',
+  './assets/images/chair-top.png',
+  './assets/images/chairBlack-front.png',
+  './assets/images/chairBlack-top.png',
+  './assets/images/chairGrey-front.png',
+  './assets/images/chairGrey-top.png',
+  './assets/images/chairHigh-top.png',
+  './assets/images/chairSit-menu.png',
+  './assets/images/chairStoolRow-top.png',
+  './assets/images/chairSwivel-top.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(PRECACHE_ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName.startsWith('vrc-pwa-') && cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+
+  if (url.origin !== self.location.origin) {
+    return;
+  }
+
+  if (url.pathname === '/heartbeat') {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request).then((response) => {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    })
+  );
+});
